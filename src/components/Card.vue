4aab8960e16438b057625cb063eb75de1c9b3f17
shelly <lufunomulaudzi00@gmail.com>
2023-09-01 19:47:19 +0200

    Comminting unfished quiz app

---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="card">
+        <img :src="quizzy.img" alt="">
+        <div class="card-text">
+          <h2>{{ quizzy.name }}</h2>
+          <p>{{ quizzy.questions.length }} questions</p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script setup>
+//import { defineProps } from "vue";
+
+const { quizzy} = defineProps(['quizzy'])
+</script>
+
+<style scoped>
+.card {
+  width: 300px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 2px darkgray;
+  cursor: pointer;
+ 
+}
+
+.card img {
+  width: 100%;
+  margin: 0;
+  height: 130x;
+}
+.card:hover {
+  border: 1px solid #777;
+}
+
+.card .card-text {
+  padding: 0 6px;
+}
+
+.card .card-text h2 {
+  font-weight: bold;
+}
+
+</style>
